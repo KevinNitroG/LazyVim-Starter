9fe7701bc73622c59c31542eebfa4393132ff45e
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-03-31 09:51:49 +0700

    fix(cloak): patterns in opts

--- lua/plugins/cloak.lua
+++ lua/plugins/cloak.lua
@@ -1,26 +1,31 @@
 return {
-  "laytan/cloak.nvim",
-  event = "VeryLazy",
-  keys = {
-    {
-      "<leader>uh",
-      ":CloakToggle<CR>",
-      desc = "Toogle cloak",
-    },
-  },
-  opts = {
-    cloak_length = "10",
-    patterns = {
-      file_pattern = {
-        "**/*env*.{json,yml,yaml,toml}",
-        "**/*account*.{json,yml,yaml,toml}",
-        "**/*credential*.{json,yml,yaml,toml}",
-        "**/*password*.{json,yml,yaml,toml}",
-        "**/*config*.{json,yml,yaml,toml}",
+  {
+    "laytan/cloak.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>uh",
+        ":CloakToggle<CR>",
+        desc = "Toogle cloak",
       },
-      cloak_pattern = {
-        "=.+",
-        "-.+",
+    },
+    opts = {
+      cloak_length = "5",
+      patterns = {
+        {
+          file_pattern = {
+            "**/*env*.{json,yml,yaml,toml}",
+            "**/*account*.{json,yml,yaml,toml}",
+            "**/*credential*.{json,yml,yaml,toml}",
+            "**/*password*.{json,yml,yaml,toml}",
+            "**/*config*.{json,yml,yaml,toml}",
+          },
+          cloak_pattern = {
+            "=.+",
+            "-.+",
+            ":.+",
+          },
+        },
       },
     },
   },
