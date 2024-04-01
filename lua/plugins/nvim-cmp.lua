1da242f815e76b5d90b52ba4e8640795519704b8
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-04-01 09:07:28 +0700

    chore(supertab): restructure and copilot

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,11 @@
 return {
   "hrsh7th/nvim-cmp",
-  -- dependencies = { "hrsh7th/cmp-emoji" },
+  dependencies = {
+    -- "hrsh7th/cmp-emoji",
+    "zbirenbaum/copilot.lua",
+    "danymat/neogen",
+    "L3MON4D3/LuaSnip",
+  },
   opts = function(_, opts)
     local cmp = require("cmp")
     opts.window = {
@@ -8,44 +13,6 @@ return {
       documentation = cmp.config.window.bordered(),
     }
     -- table.insert(opts.sources, { name = "emoji" })
-
-    -- https://www.lazyvim.org/configuration/recipes#supertab
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
-    local luasnip = require("luasnip")
-    local neogen = require("neogen")
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if neogen.jumpable() then
-          neogen.jump_next()
-        elseif cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- this way you will only jump inside the snippet region
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if neogen.jumpable(true) then
-          neogen.jump_prev()
-        elseif cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
+    opts.mapping = vim.tbl_extend("force", opts.mapping, require("config.supertab"))
   end,
 }
